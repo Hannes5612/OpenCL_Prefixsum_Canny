d2041ffb3b1a26b36a5ebcc7be55d14ec490548f
Hannes Frey <hf018@hdm-stuttgart.de>
2021-03-07 16:37:02 +0100

    comment improvements

--- ImageFX/ImageFX/ImageFX.cl
+++ ImageFX/ImageFX/ImageFX.cl
@@ -8,61 +8,19 @@
 #define LX get_local_id(0)
 #define LY get_local_id(1)
 
+// Work item position
+#define pos get_global_size(0) * get_global_id(1) + get_global_id(0)
 
-#define L_SIZE 16
+// ====== Convolution Kernel ======
+__kernel void imgfx(__global float* in, __global float* out, int dim, __global float* filterMat, int flag) {
 
-
-__kernel void imgfx(__global float* in, __global float* out, int width, int height, int dim, __global float* filterMat, int flag) {
-
-
-	//int kernel_rad = (dim - 1) / 2;
-	//if (GX > kernel_rad && GX < width - kernel_rad && GY>kernel_rad && GY < height - kernel_rad) {
-	//	float rSum = 0, kSum = 0;
-	//	for (uint i = 0; i < dim; i++)
-	//	{
-	//		for (uint j = 0; j < dim; j++)
-	//		{
-	//			int pixelPosX = GX + (i - (dim / 2));
-	//			int pixelPosY = GY + (j - (dim / 2));
-
-	//			if ((pixelPosX < 0) ||
-	//				(pixelPosX >= width) ||
-	//				(pixelPosY < 0) ||
-	//				(pixelPosY >= height)) continue;
-
-	//			float r = in[pixelPosX + pixelPosY * width];
-	//			float kernelVal = filterMat[i + j * dim];
-	//			rSum += r * kernelVal;
-
-	//			kSum += kernelVal;
-	//		}
-	//	}
-	//	if (kSum == 0) kSum = 1;
-	//	rSum /= kSum;
-	//	out[GX + GY * width] = rSum;
-	//}
-	//else {
-	//	out[GX + GY * width] = 255;
-	//}
-	//barrier(CLK_LOCAL_MEM_FENCE);
-
-	{
-	//// Die Pixel sind vom Datentyp uchar4. Ein solcher Vektor bestehend aus 4 uchar-Werten (rot, grün, blau, alpha)
-	//// kann mit einem einzelnen uchar multipliziert werden, aber nicht mit anderen Datentypen, wie z.B. einem float
-	//// Um mit einem float multiplizieren zu können, muss zuerst konvertiert werden. Zur Konvertierung von Vektordatentypen
-	//// gibt es Fukntionen.
-	//// Beispiel
-	////
-	//// float a,b;
-	//// uchar4 pix1 = in[0];
-	//// uchar4 pix2 = in[1];
-	//// uchar4 value = a*convert_float4(pix1) + b*convert_float4(pix2);
-	//// out[0] = convert_uchar4(value);
-
+	// Pixel we save our convoluted values in
 	float calculated_pixel = 0.0;
+	// Filter radius
 	int offset = (dim - 1) / 2;
-	size_t pos = width * GY + GX;
 
+
+	// Filter index incremented during loop
 	int filter_index = 0;
 
 	// Go through filter matrix indices and calculate endpixel
@@ -71,7 +29,7 @@ __kernel void imgfx(__global float* in, __global float* out, int width, int heig
 			if (x_axis_index >= 0 && y_axis_index >= 0										// Make sure to not multiply by negative values -> would skip whole x=0 and y=0
 				&& x_axis_index <= GY * GW + GX && y_axis_index <= GY * GW + GX) 
 			{									
-				int one_dim_ind = y_axis_index * width + x_axis_index;						// Calculate used index
+				int one_dim_ind = y_axis_index * GW + x_axis_index;							// Calculate used index
 				float pixel = in[one_dim_ind];												// Get pixel values of index
 				calculated_pixel += filterMat[filter_index] * pixel;						// Add weighted values to end pixel
 			}
@@ -79,14 +37,12 @@ __kernel void imgfx(__global float* in, __global float* out, int width, int heig
 		}
 	}
 	
-	// Check if we are at border pixels
+	// Check if we are at border pixels -> set black if
 	if (GX < offset || GY < offset || GX >= (GW - offset) || GY >= (GH - offset)) {
 		calculated_pixel = 0.0;
 	} 
 
-	barrier(CLK_LOCAL_MEM_FENCE);
-
-	// clamp value in allowed range
+	// Clamp value between max and min value
 	calculated_pixel = (float)clamp((float)calculated_pixel, (float)-255, (float)255);
 
 	// Apply scaling or absolution
@@ -99,51 +55,63 @@ __kernel void imgfx(__global float* in, __global float* out, int width, int heig
 
 	// Write in output buffer
 	out[pos] = calculated_pixel;
-}}
+}
 
-
+// ====== Greyscaling kernel ======
 __kernel void to_grey(__global uchar4* in, __global float* out) {
-
+	// Convert uchar4 to float4 for calculations
 	float4 pixel = convert_float4(in[GX + GY * GW]);
+	// Calculate greyscaled value
 	float average_rgb = (pixel.x + pixel.y + pixel.z) / 3;
 
+	// Write only float values to ease up succeeding steps
 	out[GX + GY * GW] = average_rgb;
 }
 
+// ====== Convert float to uchar4 kernel ======
 __kernel void to_uchar(__global float* in, __global uchar4* out) {
-	float pixel = in[GY * GW + GX];
-	out[GY * GW + GX] = (uchar4)(pixel, pixel, pixel, 1);
+	// Get input value 
+	float pixel = in[pos];
+	// Turn single color value to turn into BGRA pixel
+	out[pos] = (uchar4)(pixel, pixel, pixel, 1);
 }
 
-
+// ====== Calculate absolute edgestrengths kernel ====== 
 kernel void abs_edge(global float* x_sobel, global float* y_sobel, global float* out) {
-
-	size_t pos = GW * GY + GX;
-
+	
+	// Get input values
 	float x = x_sobel[pos];
 	float y = y_sobel[pos];
 
+	// Write to output buffer
 	out[pos] = min(255.0f, hypot(x, y));
 }
 
+// ====== Non maximum suppression kernel ======
 __kernel void nms(__global float* in, __global float* out) {
-	size_t pos = GY * GW + GX;
+
+	// get input pixel
 	float pixel = in[pos];
+
+	// save edges
 	float newPixel = 0.0;
 	
+	// Check for left and right values
 	if (GX > 0 && GX < GW - 1) {
 		float leftPixel = in[pos - 1];
 		float rightPixel = in[pos + 1];
+		// check if our current position has a higher value than the neighbour pixels
 		if (leftPixel <= pixel && rightPixel < pixel) {
 			newPixel = pixel;
 		}
 	}
 
-	barrier(CLK_LOCAL_MEM_FENCE);
 
+	// Check for top and bottom values
 	if (GY > 0 && GY < GH - 1) {
 		float topPixel = in[pos - GW];
 		float bottomPixel = in[pos + GW];
+		// check if our current position has a higher value than the neighbour pixels
 		if (topPixel <= pixel && bottomPixel < pixel) {
 			newPixel += pixel;
 		}
@@ -159,7 +127,6 @@ __kernel void hysterese(__global float* in, __global float* out) {
 	float high = 80;
 
 	float edge = 255;
-	int pos = GY * GW + GX;
 
 	float pixel = in[pos];
 
--- ImageFX/ImageFX/ImageFX.cpp
+++ ImageFX/ImageFX/ImageFX.cpp
@@ -59,6 +59,8 @@ struct ImgFXWindow : Window {
     int32_t *outbuf;
     SDL_Surface *image;
 
+    cl_int status = 0;
+
 	OpenCLMgr *mgr;
 
     ImgFXWindow() {
@@ -111,41 +113,30 @@ struct ImgFXWindow : Window {
 
 	virtual void onSave() {
 		// ================================================
-		// ==== called when clicking the "save" button ====
+		// ==== Called when clicking the "save" button ====
 		// ================================================
 		saveBMP(outputFilename);
 	}
 
 
-    virtual cl_mem applyFilter(cl_mem in_buffer,  cl_float* FilterMat, cl_int dim, cl_int flag){
-        size_t gdims[] = { image->w, image->h };
+    virtual cl_mem applyFilter(cl_mem in_buffer,  cl_float* FilterMat, cl_int dim, cl_int flag, size_t* global_size){
 
+        cl_mem outBuffer = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(cl_float)*image->w*image->h, NULL, NULL);
+        cl_mem filterMem = clCreateBuffer(mgr->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_int) * dim * dim, FilterMat, NULL);
 
-        cl_mem out_buffer = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(cl_float)*image->w*image->h, NULL, NULL);
-        cl_mem filtermem = clCreateBuffer(mgr->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_int) * dim * dim, FilterMat, NULL);
-
-        cl_int status = 0;
         status = clSetKernelArg(procKernel, 0, sizeof(in_buffer), &in_buffer);
-		status |= clSetKernelArg(procKernel, 1, sizeof(out_buffer), &out_buffer);
-		status |= clSetKernelArg(procKernel, 2, sizeof(cl_int), &image->w);
-		status |= clSetKernelArg(procKernel, 3, sizeof(cl_int), &image->h);
-        status |= clSetKernelArg(procKernel, 4, sizeof(cl_int), &dim);
-        status |= clSetKernelArg(procKernel, 5, sizeof(filtermem), &filtermem);
-        status |= clSetKernelArg(procKernel, 6, sizeof(cl_int), &flag);
+		status |= clSetKernelArg(procKernel, 1, sizeof(outBuffer), &outBuffer);
+        status |= clSetKernelArg(procKernel, 2, sizeof(cl_int), &dim);
+        status |= clSetKernelArg(procKernel, 3, sizeof(filterMem), &filterMem);
+        status |= clSetKernelArg(procKernel, 4, sizeof(cl_int), &flag);
 
-        if (status)
-			throw "set kernel arg";
-        
-        status |= clEnqueueNDRangeKernel(mgr->commandQueue, procKernel, 2, NULL, gdims, NULL, 0, NULL, NULL);
-        std::cout << status;
+        status |= clEnqueueNDRangeKernel(mgr->commandQueue, procKernel, 2, NULL, global_size, NULL, 0, NULL, NULL);
 
-        return out_buffer;
+        return outBuffer;
     }
 
 
-    virtual cl_mem applyGrey(cl_mem in_buffer) {
-        size_t gdims[] = { image->w, image->h };
-        cl_int status = 0;
+    virtual cl_mem applyGrey(cl_mem in_buffer, size_t *global_size) {
 
         // creating and setting parameters for grey_buffer kernel call
         cl_mem greyMem = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(cl_float) * image->w * image->h, NULL, NULL);
@@ -153,68 +144,53 @@ struct ImgFXWindow : Window {
         status |= clSetKernelArg(greyKernel, 1, sizeof(greyMem), &greyMem);
 
         // starting the greyscale kernel
-        status |= clEnqueueNDRangeKernel(mgr->commandQueue, greyKernel, 2, NULL, gdims, NULL, 0, NULL, NULL);
+        status |= clEnqueueNDRangeKernel(mgr->commandQueue, greyKernel, 2, NULL, global_size, NULL, 0, NULL, NULL);
 
         return greyMem;
     }
 
-    virtual cl_mem applyAbsEdge(cl_mem xSobelMem, cl_mem ySobelMem) {
+    virtual cl_mem applyAbsEdge(cl_mem xSobelMem, cl_mem ySobelMem, size_t* global_size) {
         // Create Buffer for abs strengths and gradients
         cl_mem absEdgeMem = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(cl_float) * image->w * image->h, NULL, NULL);
-
-        cl_int status = 0;
-        size_t gdims[] = { image->w, image->h };
 
         status = clSetKernelArg(absEdgeKernel, 0, sizeof(xSobelMem), &xSobelMem);
         status |= clSetKernelArg(absEdgeKernel, 1, sizeof(ySobelMem), &ySobelMem);
         status |= clSetKernelArg(absEdgeKernel, 2, sizeof(absEdgeMem), &absEdgeMem);
 
-        status = clEnqueueNDRangeKernel(mgr->commandQueue, absEdgeKernel, 2, NULL, gdims, NULL, 0, NULL, NULL);
+        status = clEnqueueNDRangeKernel(mgr->commandQueue, absEdgeKernel, 2, NULL, global_size, NULL, 0, NULL, NULL);
 
         return absEdgeMem;
     }
 
-    virtual cl_mem applyNms(cl_mem absEdgeMem) {
+    virtual cl_mem applyNms(cl_mem absEdgeMem, size_t* global_size) {
         cl_mem nmsMem = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(cl_float) * image->w * image->h, NULL, NULL);
-
-        cl_int status = 0;
-        size_t gdims[] = { image->w, image->h };
-
 
         status = clSetKernelArg(nmsKernel, 0, sizeof(absEdgeMem), &absEdgeMem);
         status |= clSetKernelArg(nmsKernel, 1, sizeof(nmsMem), &nmsMem);
 
-        status = clEnqueueNDRangeKernel(mgr->commandQueue, nmsKernel, 2, NULL, gdims, NULL, 0, NULL, NULL);
+        status = clEnqueueNDRangeKernel(mgr->commandQueue, nmsKernel, 2, NULL, global_size, NULL, 0, NULL, NULL);
         
         return nmsMem;
     }
 
-    virtual cl_mem toUchar(cl_mem floatMem) {
+    virtual cl_mem toUchar(cl_mem floatMem, size_t* global_size) {
         cl_mem ucharMem = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(int32_t) * image->w * image->h, NULL, NULL);
-        
-        cl_int status = 0;
-        size_t gdims[] = { image->w, image->h };
-
 
         status = clSetKernelArg(ucharKernel, 0, sizeof(floatMem), &floatMem);
         status |= clSetKernelArg(ucharKernel, 1, sizeof(ucharMem), &ucharMem);
 
-        status = clEnqueueNDRangeKernel(mgr->commandQueue, ucharKernel, 2, NULL, gdims, NULL, 0, NULL, NULL);
+        status = clEnqueueNDRangeKernel(mgr->commandQueue, ucharKernel, 2, NULL, global_size, NULL, 0, NULL, NULL);
 
         return ucharMem;
     }
 
-    virtual cl_mem applyHysterese(cl_mem floatMem) {
+    virtual cl_mem applyHysterese(cl_mem floatMem, size_t* global_size) {
         cl_mem hystMem = clCreateBuffer(mgr->context, CL_MEM_READ_WRITE, sizeof(cl_float) * image->w * image->h, NULL, NULL);
-
-        cl_int status = 0;
-        size_t gdims[] = { image->w, image->h };
-
 
         status = clSetKernelArg(hystereseKernel, 0, sizeof(floatMem), &floatMem);
         status |= clSetKernelArg(hystereseKernel, 1, sizeof(hystMem), &hystMem);
 
-        status = clEnqueueNDRangeKernel(mgr->commandQueue, hystereseKernel, 2, NULL, gdims, NULL, 0, NULL, NULL);
+        status = clEnqueueNDRangeKernel(mgr->commandQueue, hystereseKernel, 2, NULL, global_size, NULL, 0, NULL, NULL);
 
         return hystMem;
     }
@@ -224,29 +200,27 @@ struct ImgFXWindow : Window {
         // ==== called when clicking the "apply" button ====
         // ==================================================
         if (!outbuf || !image) return;
-        size_t gdims[] = { image->w, image->h };
-        cl_int status = 0;
+        // Dimension of our filters
         cl_int dim = 3;
-        cl_int flag = 1; // 1 if absolute values wanted, 0 if scaled values wanted
+        // Flag to determine whether absolute (1) values are wanted, or if scaled (0) values wanted
+        cl_int flag = 1;
+        // Calculate global work size to pass into methods
+        size_t gdims[] = { image->w, image->h };
         
-
         // ==== 1. Greyscale image ====
-        cl_mem greyMem = applyGrey(inmem);
-
+        cl_mem greyMem = applyGrey(inmem, gdims);
 
         // ==== 2. Gauss smoothing ====
         // create smoothin filter
         cl_float gaussFilterMat[9] = { 0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625 };
 
         // applying the smoothing filter on the greyscaled picture
-        cl_mem gaussMem = applyFilter(greyMem, gaussFilterMat, dim, flag);
+        cl_mem gaussMem = applyFilter(greyMem, gaussFilterMat, dim, flag, gdims);
         for (int i = 0; i < 5; i++) {
-        gaussMem = applyFilter(gaussMem, gaussFilterMat, dim, flag);
+            gaussMem = applyFilter(gaussMem, gaussFilterMat, dim, flag, gdims);
         }
-
         // release not anymore needed memory buffer of greyscaled image
-        // clReleaseMemObject(greyMem);
-
+        clReleaseMemObject(greyMem);
 
         // ==== 3. x- and y-Sobel edge detection ====
         // x and y sobel filters
@@ -254,47 +228,49 @@ struct ImgFXWindow : Window {
         cl_float sobelYFilterMat[9] = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
 
         //applying both filters on the smoothed picture
-        cl_mem xSobelMem = applyFilter(gaussMem, sobelXFilterMat, dim, flag);
-        cl_mem ySobelMem = applyFilter(gaussMem, sobelYFilterMat, dim, flag);
+        cl_mem xSobelMem = applyFilter(gaussMem, sobelXFilterMat, dim, flag, gdims);
+        cl_mem ySobelMem = applyFilter(gaussMem, sobelYFilterMat, dim, flag, gdims);
 
         // release not anymore needed memory buffer of smoothed image memory buffer
-        //clReleaseMemObject(gaussMem);
+        clReleaseMemObject(gaussMem);
 
+        // ==== 4. Calculating absolute edgestrength, release previous buffer ====
+        cl_mem absEdgeMem = applyAbsEdge(xSobelMem, ySobelMem, gdims);
+        clReleaseMemObject(xSobelMem);
+        clReleaseMemObject(ySobelMem);
 
-        // ==== 4. Calculating absolute edgestrength ====
-        cl_mem absEdgeMem = applyAbsEdge(xSobelMem, ySobelMem);
+        // ==== 5. Applying non-maximum supression, release previous buffer ======
+        cl_mem nmsMem = applyNms(absEdgeMem, gdims);
+        clReleaseMemObject(absEdgeMem);
 
+        // ==== 6. Apply hyterese claculations, release previous buffer ==========
+        cl_mem hystMem = applyHysterese(nmsMem, gdims);
+        clReleaseMemObject(nmsMem);
 
-        // ==== 5. Applying non-maximum supression
-        cl_mem nmsMem = applyNms(absEdgeMem);
-        // clReleaseMemObject(*absEdgeMem);
-
-        // ==== 6. Apply hyterese claculations
-        cl_mem hystMem = applyHysterese(nmsMem);
-        //clReleaseMemObject(nmsMem);
-
-        // ==== Lastly convert back to uchar
-        cl_mem ucharMem = toUchar(hystMem);
-        //clReleaseMemObject(hystMem);
-
-
+        // ==== Lastly convert back to uchar, release previous buffer ============
+        cl_mem ucharMem = toUchar(hystMem, gdims);
+        clReleaseMemObject(hystMem);
 
         // reading the result
         status = clEnqueueReadBuffer(mgr->commandQueue, ucharMem, CL_TRUE, 0, sizeof(*outbuf) * image->w * image->h, outbuf, 0, NULL, NULL);
-        // std::cout << status;
-        float* outbuf_2;
-        outbuf_2 = new float[image->w * image->h];
 
-        // reading data for debugging information
-        //status = clEnqueueReadBuffer(mgr->commandQueue, absEdgeMem[1], CL_TRUE, 0, sizeof(*outbuf_2) * image->w * image->h, outbuf_2, 0, NULL, NULL);
+        // Debug code
+        {
+            // std::cout << status;
+            //float* outbuf_2;
+            //outbuf_2 = new float[image->w * image->h];
+        
+            // reading data for debugging information
+            //status = clEnqueueReadBuffer(mgr->commandQueue, absEdgeMem[1], CL_TRUE, 0, sizeof(*outbuf_2) * image->w * image->h, outbuf_2, 0, NULL, NULL);
 
-        //std::cout << *std::max_element(outbuf_2, outbuf_2 + (image->w * image->h)) << "\n";
-        //std::cout << *std::min_element(outbuf_2, outbuf_2 + (image->w * image->h));
+            //std::cout << *std::max_element(outbuf_2, outbuf_2 + (image->w * image->h)) << "\n";
+            //std::cout << *std::min_element(outbuf_2, outbuf_2 + (image->w * image->h));
 
-        //int j = image->w * image->h;
-        //for (int i = 0; i < j; i++) {
-        //std::cout << outbuf_2[i] << '\n';
-        //}
+            //int j = image->w * image->h;
+            //for (int i = 0; i < j; i++) {
+            //std::cout << outbuf_2[i] << '\n';
+            //}
+        }
 
     }
 };
